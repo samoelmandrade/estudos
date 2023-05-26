d6aa1d7153c8f00de1cc35cc9aa5d50b5d8d4f99
samoelmandrade <101720444+SamoelSLC@users.noreply.github.com>
2023-05-25 22:59:25 -0300

    MAIN

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,15 +1,32 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI()
+import sqlite3
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+con = sqlite3.connect("D:\\curso\\sqlite3\\db\\Chinook")
+cur = con.cursor()
+
+def INSERT_TABLE(titulo,ano,score):
+    data = [
+    (titulo, ano, score)
+]
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    con.commit() 
+    return True
+
+'''titulo = (input("DIGITE O TITULO: "))
+ano = int(input("qual O ANO: "))
+score = int(input("SCORE: "))
+
+retorno = INSERT_TABLE(titulo=titulo,ano=ano,score=score)
+print(retorno)
+'''
+print('--------------------------')
+nome = 'NERCI'
+
+res = con.execute("SELECT title, year, score FROM movie mo where mo.title like name = '(nome)'" )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+title, year , score = res.fetchone()
+print ('RETORNO DA TABELA')
+print(f'TITULO:  {title!r}, ANO:  {year} E PONTUAÇAO:  {score}')
