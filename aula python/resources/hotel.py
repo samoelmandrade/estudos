fc61f2b5682d2673db357ea710bbdd8d2feddb21
samoelmandrade <101720444+SamoelSLC@users.noreply.github.com>
2023-08-28 20:23:04 -0300

    te

--- aula python/resources/hotel.py
+++ aula python/resources/hotel.py
@@ -1,4 +1,6 @@
-from flask_restful import Resource 
+from flask import Flask 
+from flask_restful import Resource, reqparse
+
 
 hoteis = [
         {
@@ -25,5 +27,57 @@ hoteis = [
 ]
 
 class Hoteis(Resource):
+
     def get(self):
         return {'hoteis': hoteis}
+    
+class Hotel(Resource):   
+    
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+    
+        def find_hotel(hotel_id):
+            for hotel in hoteis:
+                if hotel['hotel_id']==hotel_id:
+                    return hotel
+            return None
+    
+        def get(self, hotel_id):
+            hotel= Hotel.find_hotel(hotel_id)
+            if hotel: 
+                return hotel
+            return {'message':'not found'}
+            
+        def post(self,hotel_id):
+
+            dados =Hotel.argumentos.parse_args()
+        
+            novo_hotel={
+            
+            'hotel_id':hotel_id,
+            'nome': dados['nome'],
+            'estrelas':dados['diaria'],
+            'cidade': dados['cidade']
+            }
+            hoteis.append(novo_hotel)
+            return novo_hotel,200
+    
+    
+        def put(self,hotel_id):
+            
+            dados =Hotel.argumentos.parse_args()
+            
+            novo_hotel={'hotel_id':hotel_id, **dados}
+                       
+            hotel = Hotel.find_hotel(hotel_id)
+            if hotel:
+                hotel.update(novo_hotel)
+                return novo_hotel,200
+            hoteis.append(novo_hotel)       
+            return novo_hotel,201
+        
+        def delete(self):
+            return {'hoteis': hoteis}
